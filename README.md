074fd5838194252f2f70e287c986f3dab608117c
Brian Stone <brianpatrickstone@gmail.com>
2015-11-12 20:44:15 -0700

    completed code and files

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# words_test
+#words_test
+By Brian Stone
+
+##What is word_test?
+A program used to solve the following problem:
+Write a program which, given a dictionary, generates two output files, 'sequences' and 'words'. 'sequences' should contain every sequence of four letters (A-z) that appears in exactly one word of the dictionary, one sequence per line. 'words' should contain the corresponding words that contain the sequences, in the same order, again one per line.
+
+###Requirements:
+1. Ruby 2.0+ installed
+
+###Getting Started
+1. Clone `words_test`
+2. `ruby word_program.rb`
+3. Pass in `dictionary.txt`
+4. View created files
+
+###Additional Files
+`dictionary.txt`
+Contains a large number of words and should be used to achieve the final solution.
+
+`test_dict.txt`
+Contains a small number of words used for testing on a smaller scale.
+
+`word_program_test.rb`
+Contains similar code as word_program.rb, but includes small tests for clarification. Can be run seperately if needed.
+
+`sequences`
+Sequences is created after word_test is finished running and contains the desired 4 letter sequences.
+
+`words`
+Words is created after word_test is finished running and contains the words corresponding to each 4 letter sequence.
--- word_program_test.rb
+++ word_program_test.rb
@@ -1,24 +1,28 @@
 #clear screen for clarity
 system "clear"
-
 #ask for file from user
 puts "Hello. Please enter the file to read: "
 txt = gets.chomp
-
-#create files with authorized write status to be populated
 words_file = File.open('words', 'w')
 sequences_file = File.open('sequences', 'w')
-
 #initialize a blank array for words
 a = []
-
-#open the user given text file and add each line to the initialized array 'a'
+arr = []
+#open the user given text file and add each line to the initialized array
 txt_file = File.open(txt) do |f|
 	f.each_line do |line|
 		if line.chomp.length >= 4
 			a << line.chomp
+		else
+			arr << line.chomp
 		end
 	end
+	# verifying file was imported with all words.
+	puts "There are #{a.count} words all together"
+
+	puts "There are #{a.count} words with 4 or more characters"
+	puts "There are #{arr.count} words less than 4 characters"
+
 
 	#remove 4 letter sequences and place them into a hash
 	sequences = Hash.new { |hash, key| hash[key] = [] }
@@ -31,17 +35,20 @@ txt_file = File.open(txt) do |f|
 		end
 	end
 
-	#iterate over sequences and place sequences and words in the correct files.
+	puts "There are #{sequences.count} sequences."
+	puts sequences
+	puts sequences.size
+	#iterate over hash and place hash in 'sequences' file and key in 'words' file
 	sequences.each do |key, value|
 		if value.uniq.size == 1
+			puts "#{key}\t#{value.uniq.first}"
 			words_file.puts value.uniq.first
 			sequences_file.puts key
 		end
 	end
 	words_file.close
 	sequences_file.close
-
-	#alert user of program completion.
 	puts "Done! Please refer to the created 'sequences' and 'words' files for results."
+
 
 end
